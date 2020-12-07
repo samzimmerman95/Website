d8c8c52f61c22e5c989af3f45ac153293d83333b
samzimmerman95 <samzimmerman95@gmail.com>
2020-12-06 19:53:43 -0800

    Reponsive adjustments

--- src/Footer.tsx
+++ src/Footer.tsx
@@ -5,12 +5,12 @@ export default function Footer() {
   return (
     <footer className="container border-top mt-5">
       <div className="row pt-2">
-        <div className="col-sm ">
+        <div className="col-md d-md-flex align-items-center text-center justify-content-start">
           <Link className="navbar-brand text-dark" to="/">
             Sam Zimmerman
           </Link>
         </div>
-        <div className="col-sm d-flex justify-content-center align-items-center">
+        <div className="col-md d-md-flex text-center justify-content-center align-items-center">
           <a
             className="link-icon"
             href="https://www.instagram.com/sammyzimm/"
@@ -42,7 +42,7 @@ export default function Footer() {
           </a>
         </div>
 
-        <div className="col-sm d-flex align-items-center small justify-content-end">
+        <div className="col-md d-md-flex align-items-center text-center small justify-content-end">
           © Sam Zimmerman 2020
         </div>
       </div>
