25dd194bc885ee8f36a5e17a72eae23c59cd6262
samzimmerman95 <samzimmerman95@gmail.com>
2021-03-03 09:57:33 -0800

    Wood Island Adv

--- src/adventure/stories/2017Wood.tsx
+++ src/adventure/stories/2017Wood.tsx
@@ -2,5 +2,16 @@ import React from "react";
 import Images from "../../assets/images/adventures/2017Wood/Images";
 import * as Builder from "../storyBuilder";
 export default function WoodIsland() {
-  return <div>{Builder.title(Images[0], "Wood Island", "May 2017")}</div>;
+  return (
+    <div>
+      {Builder.title(Images[2], "Wood Island", "May 2017")}
+      {Builder.twoCols(Images[0], Images[3])}
+      {Builder.imageLTextR(
+        Images[1],
+        "The Wood Island Life Saving Station was built in 1908 at the mouth of the Piscataque River in Kittery Point, Maine. After decades of neglect, it is finally undergoing restoration. This summer Pierce was tasked with driving the construction crew out to the island in the mornings. We often went out to explore this historic site for ourselves."
+      )}
+      {Builder.fullScreen(Images[4])}
+      {Builder.fullScreen(Images[5])}
+    </div>
+  );
 }
