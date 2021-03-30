23fc9ad931ef1b71c16c3afd26b2142934f967bf
samzimmerman95 <samzimmerman95@gmail.com>
2021-03-30 15:57:15 -0700

    Mammoth 2019

--- src/adventure/stories/2019Mammoth.tsx
+++ src/adventure/stories/2019Mammoth.tsx
@@ -2,5 +2,17 @@ import React from "react";
 import Images from "../../assets/images/adventures/2019Mammoth/Images";
 import * as Builder from "../storyBuilder";
 export default function Mammoth() {
-  return <div>{Builder.title(Images[0], "Mammoth", "December 2019")}</div>;
+  return (
+    <div>
+      {Builder.title(Images[6], "Mammoth", "December 2019")}
+      {Builder.twoCols(Images[0], Images[3])}
+      {Builder.bigLeftSmallRight(Images[5], Images[4])}
+      {Builder.smallLeftBigRight(Images[1], Images[2])}
+      {Builder.fullImage(Images[7])}
+      {Builder.imageLTextR(
+        Images[8],
+        `Evening light on snowy mountains is one of my favorite sights.`
+      )}
+    </div>
+  );
 }
