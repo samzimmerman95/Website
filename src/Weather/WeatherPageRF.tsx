93c74e70d112bb5db0a5e6a4d586361573d07906
samzimmerman95 <samzimmerman95@gmail.com>
2021-03-31 11:51:42 -0700

    Toggle view for different time steps

--- src/Weather/WeatherPageRF.tsx
+++ src/Weather/WeatherPageRF.tsx
@@ -26,6 +26,9 @@ async function getDataFromFirebase() {
 
 export default function WeatherPageRF() {
   const [allData, setAllData] = useState<rawData[]>([]);
+  const [lastWeekData, setLastWeekData] = useState<rawData[]>([]);
+  const [lastDayData, setLastDayData] = useState<rawData[]>([]);
+  const [currentTimeSelected, setCurrentTimeSelected] = useState("Week");
   const [latest, setLatest] = useState<rawData>({
     date: 0,
     temps: [0, 0],
@@ -35,6 +38,12 @@ export default function WeatherPageRF() {
   useEffect(() => {
     getDataFromFirebase().then((result) => {
       setAllData(result);
+      let lastWeekTime = Date.now() - 604800000;
+      let lastDayTime = Date.now() - 86400000;
+      let lastWeek = result.filter((entry) => entry.date > lastWeekTime);
+      let lastDay = lastWeek.filter((entry) => entry.date > lastDayTime);
+      setLastWeekData(lastWeek);
+      setLastDayData(lastDay);
       let lastEntry = result[result.length - 1];
       setLatest(lastEntry);
       let lastTime = new Date(lastEntry.date);
@@ -43,7 +52,15 @@ export default function WeatherPageRF() {
     });
   }, []);
 
-  useEffect(() => {}, [allData]);
+  function ChangeChart() {
+    if (currentTimeSelected === "Day") {
+      return <Chart width={800} height={500} data={lastDayData} />;
+    } else if (currentTimeSelected === "Week") {
+      return <Chart width={800} height={500} data={lastWeekData} />;
+    } else {
+      return <Chart width={800} height={500} data={allData} />;
+    }
+  }
 
   return (
     <div className="container-fluid d-flex flex-column min-vh-100">
@@ -51,8 +68,31 @@ export default function WeatherPageRF() {
       {/*Only here and p5 page since only those actually need it.*/}
       <div className="flex-grow-1">
         <NavBar />
+        <div className="d-flex justify-content-center pb-2">
+          <button
+            className="btn btn-outline-secondary mx-1 btn-sm"
+            data-bs-toggle="button"
+            onClick={() => setCurrentTimeSelected("Day")}
+          >
+            24 Hours
+          </button>
+          <button
+            className="btn btn-outline-secondary mx-1 btn-sm" //active class
+            data-bs-toggle="button"
+            onClick={() => setCurrentTimeSelected("Week")}
+          >
+            Last Week
+          </button>
+          <button
+            className="btn btn-outline-secondary mx-1 btn-sm"
+            data-bs-toggle="button"
+            onClick={() => setCurrentTimeSelected("All")}
+          >
+            All Time
+          </button>
+        </div>
         <div className="d-flex justify-content-center">
-          <Chart width={800} height={500} data={allData} />
+          <ChangeChart></ChangeChart>
         </div>
         <div className="row row-cols-1 row-cols-md-3 mt-2">
           <div className="col">Updated {timeSince} minutes ago</div>
