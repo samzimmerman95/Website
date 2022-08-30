9d75178c8ee66af121e52807d462d56dca499e8f
samzimmerman95 <samzimmerman95@gmail.com>
2022-08-30 16:23:48 -0400

    Change to lake table

--- src/Weather/WeatherPageRF.tsx
+++ src/Weather/WeatherPageRF.tsx
@@ -13,7 +13,7 @@ interface rawData {
 }
 
 async function getDataFromFirebase() {
-  var tempsRef = firebase.database().ref("/lake-test/");
+  var tempsRef = firebase.database().ref("/lake/");
   var newList: rawData[] = [];
   await tempsRef.once("value").then((snapshot) => {
     let list = Object.entries(snapshot.val());
